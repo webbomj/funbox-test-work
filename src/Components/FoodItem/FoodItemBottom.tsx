3e0002898ee1ddd7f6d541712531c7ece5a88d4c
Andrey Lapin <test@test.ru>
2022-06-18 13:26:31 +0400

    fixed styles to blocked card

--- src/Components/FoodItem/FoodItemBottom.tsx
+++ src/Components/FoodItem/FoodItemBottom.tsx
@@ -3,7 +3,7 @@ import { FoodItemBottomProps } from '../../types/FoodItemBottom';
 import s from "./FoodItem.module.css"
 
 const FoodItemBottom:FC<FoodItemBottomProps> = ({isSelected, isBlocked, taste, description}) => {
-  if (isBlocked) return <div className={s.bottomBlocked}>Печалька, {taste} закончился</div>
+  if (isBlocked) return <div className={s.bottomBlocked}>Печалька, {taste} закончился.</div>
   if (isSelected && !isBlocked) return <>description</>
   else return  <>Чего сидишь? Порадуй котэ, <a href="#">купи.</a></>
 };
